de48a5b31f0c800159eddc47d6123f9bc053bb77
TheThirdWave <aldentesinferno@gmail.com>
2023-10-05 09:27:34 -0500

    Got FK Spine working

--- src/crig_maya/modules/ikfkSpline_module.py
+++ src/crig_maya/modules/ikfkSpline_module.py
@@ -18,10 +18,10 @@ class IKFKSpine(maya_base_module.MayaBaseModule):
         # Create the bind joints that the stuff in the "controls_GRP" will drive.  These should not have any actual puppetry logic in them, they should be driven by puppet joints.
         cmds.select(self.baseGroups['deform_group'])
         num_joints = 6
-        self.bind_joints = [cmds.joint(self.baseGroups['deform_group'], name='{0}_{1}_base_BND_JNT'.format(self.prefix, self.name), position=(0, 0, 0))]
+        self.bind_joints = [cmds.joint(self.baseGroups['deform_group'], name='{0}_{1}_base_BND_JNT'.format(self.prefix, self.name), position=(0, 0, 0), scaleCompensate=False)]
         cmds.xform(self.bind_joints[0], rotation=(0, 0, 90))
         for joint_idx in range(num_joints - 1):
-            self.bind_joints.append(cmds.joint(self.bind_joints[joint_idx], name='{0}_{1}_{2}_BND_JNT'.format(self.prefix, self.name, joint_idx + 1), position=(1, 0, 0), relative=True))
+            self.bind_joints.append(cmds.joint(self.bind_joints[joint_idx], name='{0}_{1}_{2}_BND_JNT'.format(self.prefix, self.name, joint_idx + 1), position=(1, 0, 0), relative=True, scaleCompensate=False))
         self.bind_joints[-1] = cmds.rename(self.bind_joints[-1], '{0}_{1}_end_BND_JNT'.format(self.prefix, self.name))
 
         # We have to initialize the components input/output custom attrs so they can be connected later, even if the component rig hasn't been created yet.
@@ -53,19 +53,141 @@ class IKFKSpine(maya_base_module.MayaBaseModule):
         fk_group = cmds.group(name='{0}_{1}_fkspine_HOLD_GRP'.format(self.prefix, self.name), parent=self.baseGroups['placement_group'], empty=True)
         parent = fk_group
         fk_joints = []
+        fk_base_controls = []
+        fk_base_place_groups = []
         idx = 0
         for joint in self.bind_joints:
             fk_joints.append(python_utils.duplicateBindJoint(joint, parent, 'FK'))
             parent = fk_joints[idx]
             idx += 1
         parent = fk_joints[0]
-        fk_base_controls = []
-        fk_base_place_groups = []
         for joint in fk_joints:
             prefix, component_name, joint_name, node_purpose, node_type = python_utils.getNodeNameParts(joint)
             control_place_group, joint_control = python_utils.makeDirectControl('{0}_{1}_{2}_{3}_{4}_{5}'.format(prefix, component_name, joint_name, 'FK', 'CTL', 'CRV'), joint, 1.0, 'square')
             fk_base_controls.append(joint_control)
             fk_base_place_groups.append(control_place_group)
+
+        # Implement FK Squash and Stretch
+        # As control Y translate increases, the X and Z scale values should decrease. (and vice-versa)
+        # We calculate a Y translate for both the current joint and the next joint (if current joint is not the end joint)
+        # Y translate = a(LSx-Sx)
+        # where:
+        #   a = Scale factor
+        #   L = Length value
+        #   Sx = is either S1 or S2
+        #   S1 = distance between current joint and previous joint (0 if base joint)
+        #   S2 = distance between current joint and next joint (0 if end joint)
+        #
+        # We only calculate scale values for the current joint.
+        # X and Z scale values = S/(S + b(LS-S))
+        # where:
+        #   b = Scale factor
+        #   S = S1 + S2
+        multByScaleFactorNodes = []
+        roughControlHookNodes = []
+        for i in range(len(fk_base_place_groups)):
+            # Create squash and stretch parent group
+            prefix, component_name, joint_name, node_purpose, node_type = python_utils.getNodeNameParts(fk_base_controls[i])
+            new_sas_group = cmds.group(fk_base_controls[i], name='{0}_{1}_{2}_{3}_{4}_{5}'.format(prefix, component_name, joint_name, 'SAS', 'PAR', 'GRP'))
+            cmds.matchTransform(new_sas_group, fk_base_controls[i], piv=True)
+
+            # Add length and scale factor attributes to the FK control.
+            currentControl = fk_base_controls[i]
+            cmds.select(currentControl)
+            cmds.addAttr(longName='Length', attributeType='float', defaultValue=1.0, minValue=0.0, keyable=True, hidden=False)
+            cmds.addAttr(longName='ScaleA', attributeType='float', defaultValue=1.0, hidden=False, keyable=True)
+            cmds.addAttr(longName='ScaleB', attributeType='float', defaultValue=1.0, hidden=False, keyable=True)
+
+            # Get starting distances between joints.
+            currentJoint = fk_joints[i]
+            distance1 = 0
+            if i > 0:
+                previousJoint = fk_joints[i - 1]
+                distance1 = python_utils.getTransformDistance(currentJoint, previousJoint)
+            distance2 = 0
+            if i < len(fk_base_place_groups) - 1:
+                nextJoint = fk_joints[i + 1]
+                distance2 = python_utils.getTransformDistance(currentJoint, nextJoint)
+
+            totalDistance = distance1 + distance2
+
+            # Set up math nodes for the rough controls to hook into later.
+            roughControlLengthDiff = cmds.shadingNode('floatMath', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '9_SAS_CMATH'), asUtility=True)
+            cmds.select(roughControlLengthDiff)
+            cmds.setAttr('{0}.operation'.format(roughControlLengthDiff), 1)
+            cmds.setAttr('{0}.floatA'.format(roughControlLengthDiff), 1)
+            cmds.setAttr('{0}.floatB'.format(roughControlLengthDiff), 1)
+            roughControlHookNodes.append(roughControlLengthDiff)
+
+            roughControlLengthAdd = cmds.shadingNode('floatMath', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '10_SAS_CMATH'), asUtility=True)
+            cmds.setAttr('{0}.operation'.format(roughControlLengthAdd), 0)
+            cmds.connectAttr('{0}.Length'.format(currentControl), '{0}.floatA'.format(roughControlLengthAdd))
+            cmds.connectAttr('{0}.outFloat'.format(roughControlLengthDiff), '{0}.floatB'.format(roughControlLengthAdd))
+
+            # Start makin' math nodes babey
+            scaleMultNode = cmds.shadingNode('colorMath', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '1_SAS_CMATH'), asUtility=True)
+            cmds.setAttr('{0}.operation'.format(scaleMultNode), 2)
+            cmds.connectAttr('{0}.outFloat'.format(roughControlLengthAdd), '{0}.colorAR'.format(scaleMultNode))
+            cmds.connectAttr('{0}.outFloat'.format(roughControlLengthAdd), '{0}.colorAG'.format(scaleMultNode))
+            cmds.connectAttr('{0}.outFloat'.format(roughControlLengthAdd), '{0}.colorAB'.format(scaleMultNode))
+            cmds.setAttr('{0}.colorBR'.format(scaleMultNode), distance1)
+            cmds.setAttr('{0}.colorBG'.format(scaleMultNode), distance2)
+            cmds.setAttr('{0}.colorBB'.format(scaleMultNode), totalDistance)
+
+            diffFromStartNode = cmds.shadingNode('colorMath', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '2_SAS_CMATH'), asUtility=True)
+            cmds.setAttr('{0}.operation'.format(diffFromStartNode), 1)
+            cmds.connectAttr('{0}.outColor'.format(scaleMultNode), '{0}.colorA'.format(diffFromStartNode))
+            cmds.setAttr('{0}.colorBR'.format(diffFromStartNode), distance1)
+            cmds.setAttr('{0}.colorBG'.format(diffFromStartNode), distance2)
+            cmds.setAttr('{0}.colorBB'.format(diffFromStartNode), totalDistance)
+
+            multByScaleFactorNode = cmds.shadingNode('colorMath', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '3_SAS_CMATH'), asUtility=True)
+            cmds.setAttr('{0}.operation'.format(multByScaleFactorNode), 2)
+            cmds.connectAttr('{0}.outColor'.format(diffFromStartNode), '{0}.colorA'.format(multByScaleFactorNode))
+            cmds.connectAttr('{0}.ScaleA'.format(currentControl), '{0}.colorBR'.format(multByScaleFactorNode))
+            cmds.connectAttr('{0}.ScaleA'.format(currentControl), '{0}.colorBG'.format(multByScaleFactorNode))
+            cmds.connectAttr('{0}.ScaleB'.format(currentControl), '{0}.colorBB'.format(multByScaleFactorNode))
+            multByScaleFactorNodes.append(multByScaleFactorNode)
+
+            # We add together the Y translates of the current and previous joint's squash and stretch, create the node for that now.
+            addPrevJointDisplacement = cmds.shadingNode('floatMath', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '4_SAS_CMATH'), asUtility=True)
+            cmds.setAttr('{0}.operation'.format(addPrevJointDisplacement), 0)
+            cmds.connectAttr('{0}.outColorR'.format(multByScaleFactorNode), '{0}.floatA'.format(addPrevJointDisplacement))
+            cmds.setAttr('{0}.floatB'.format(addPrevJointDisplacement), 0)
+            if i > 0:
+                cmds.connectAttr('{0}.outColorG'.format(multByScaleFactorNodes[i - 1]), '{0}.floatB'.format(addPrevJointDisplacement))
+            
+            # Finally Connect the Y displacement to the squash and stretch parent group (I guess it would make more sense to create the group down here but whatever)
+            cmds.connectAttr('{0}.outFloat'.format(addPrevJointDisplacement), '{0}.translateY'.format(new_sas_group))
+
+            # Now move on to the X and Z scaling stuff.
+            addToStartLen = cmds.shadingNode('floatMath', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '5_SAS_CMATH'), asUtility=True)
+            cmds.setAttr('{0}.operation'.format(addPrevJointDisplacement), 0)
+            cmds.connectAttr('{0}.outColorB'.format(multByScaleFactorNode), '{0}.floatA'.format(addToStartLen))
+            cmds.setAttr('{0}.floatB'.format(addToStartLen), totalDistance)
+
+            divideStartLen = cmds.shadingNode('floatMath', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '6_SAS_CMATH'), asUtility=True)
+            cmds.setAttr('{0}.operation'.format(divideStartLen), 3)
+            cmds.setAttr('{0}.floatA'.format(divideStartLen), totalDistance)
+            cmds.connectAttr('{0}.outFloat'.format(addToStartLen), '{0}.floatB'.format(divideStartLen))
+
+            # Connect to joint X and Z scaling.
+            cmds.connectAttr('{0}.outFloat'.format(divideStartLen), '{0}.scaleX'.format(currentJoint))
+            cmds.connectAttr('{0}.outFloat'.format(divideStartLen), '{0}.scaleZ'.format(currentJoint))
+
+            # and finally invert the scaling and connect it to the parent offset matrix of the PLC
+            # group for the next joint so the scaling only effects the current joint.
+            invertScaling = cmds.shadingNode('floatMath', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '7_SAS_CMATH'), asUtility=True)
+            cmds.setAttr('{0}.operation'.format(invertScaling), 3)
+            cmds.setAttr('{0}.floatA'.format(invertScaling), 1)
+            cmds.connectAttr('{0}.outFloat'.format(divideStartLen), '{0}.floatB'.format(invertScaling))
+
+            composeMatrix = cmds.createNode('composeMatrix', name='{0}_{1}_{2}_{3}'.format(prefix, component_name, joint_name, '8_SAS_CMATH'))
+            cmds.connectAttr('{0}.outFloat'.format(invertScaling), '{0}.inputScaleX'.format(composeMatrix))
+            cmds.connectAttr('{0}.outFloat'.format(invertScaling), '{0}.inputScaleZ'.format(composeMatrix))
+
+            if i < len(fk_base_place_groups) - 1:
+                cmds.connectAttr('{0}.outputMatrix'.format(composeMatrix), '{0}.offsetParentMatrix'.format(fk_base_place_groups[i + 1]))
 
         # After making the base fk controls, make higher order controls that can smoothly rotate multiple joints.
         # Get the number of rough controls (half the regular controls rounded up.)
@@ -73,34 +195,65 @@ class IKFKSpine(maya_base_module.MayaBaseModule):
         parent = rough_control_group
         num_higher_controls = int((len(fk_base_place_groups) / 2)) + 1
         fk_rough_controls_1 = []
+        control_placement_indicies = []
+
+        # Generate rough controls and use them to drive the fine controls.
         for i in range(num_higher_controls):
             # Match each rougher control with a base control by doing stupid index math that sucks and I hate it.
+            control_placement_indicies.append(int(round((len(fk_base_place_groups) - 1.0) / (num_higher_controls - 1.0) * i)))
             j = 0
-            for group in fk_base_place_groups:
-                control_placement_index = int(round((len(fk_base_place_groups) - 1.0) / (num_higher_controls - 1.0) * i))
-                if j == control_placement_index:
+            for control in fk_base_controls:
+                if j == control_placement_indicies[i]:
                     # Get base control name components and make rough control and placement group.
-                    prefix, component_name, joint_name, node_purpose, node_type = python_utils.getNodeNameParts(group)
+                    prefix, component_name, joint_name, node_purpose, node_type = python_utils.getNodeNameParts(control)
                     rough_control = python_utils.makeSquareControl('{0}_{1}_{2}_{3}_{4}_{5}'.format(prefix, component_name, joint_name, 'RC1', 'CTL', 'CRV'), 1.5)
-                    rough_place_group = cmds.group(rough_control, name='{0}_{1}_{2}_{3}_{4}_{5}'.format(prefix, component_name, joint_name, 'RC1', 'PLC', 'GRP'), parent=parent)
-                    cmds.matchTransform(rough_place_group, group)
+                    rough_place_group = cmds.group(rough_control, name='{0}_{1}_{2}_{3}_{4}_{5}'.format(prefix, component_name, joint_name, 'RC1', 'PLC', 'GRP'))
+                    cmds.matchTransform(rough_place_group, control)
+                    cmds.parent(rough_place_group, parent)
+                    cmds.select(rough_control)
+                    cmds.addAttr(longName='Length', attributeType='float', defaultValue=1.0, minValue=0.0, keyable=True, hidden=False)
                     # Make parent group for the base control that will be controlled by the rough control.
-                    base_control_parent = cmds.listRelatives(group, parent=True)[0]
-                    base_control_new_parent = cmds.group(group, name='{0}_{1}_{2}_{3}_{4}'.format(prefix, component_name, joint_name, 'PAR', 'GRP'), parent=base_control_parent)
-                    cmds.matchTransform(base_control_new_parent, group, piv=1)
-                    python_utils.constrainTransformByMatrix(rough_control, base_control_new_parent, True)
+                    base_control_parent = cmds.listRelatives(control, parent=True)[0]
+                    base_control_new_parent = cmds.group(control, name='{0}_{1}_{2}_{3}_{4}'.format(prefix, component_name, joint_name, 'PAR', 'GRP'))
+                    cmds.matchTransform(base_control_new_parent, control, piv=True)
+                    python_utils.connectTransforms(rough_control, base_control_new_parent)
                     parent = rough_control
                     fk_rough_controls_1.append(rough_control)
                     j = j + 1
                     break
                 j = j + 1
+
+        # After creating the rough controls (and connecting them to the fine controls they drive 100%)
+        # Go back through and set up the fine controls that are driven by more than one rough control.
+        # (and still more index stuff that is dumb and non-pythonic and there's probably a much more straightforward way to go about this.)
+        cur_placement_index = 0
+        next_placement_index = 1
+        for i in range(len(fk_base_place_groups)):
+            if i == control_placement_indicies[next_placement_index]:
+                cur_placement_index = next_placement_index
+                next_placement_index += 1
+                continue
+
+            # Because we didn't connect the rough control to the length parameter in the last go around, we do it all here.
+            first_index = control_placement_indicies[cur_placement_index]
+            next_index = control_placement_indicies[next_placement_index]
+            next_control_weight = (i - first_index) / (next_index - first_index)
+            scalar_blend_node = python_utils.createScalarBlend(
+                '{0}.Length'.format(fk_rough_controls_1[cur_placement_index]),
+                '{0}.Length'.format(fk_rough_controls_1[next_placement_index]),
+                '{0}.floatA'.format(roughControlHookNodes[i]),
+                next_control_weight)
             
-        
 
+            if i == control_placement_indicies[cur_placement_index]:
+                continue
 
+            prefix, component_name, joint_name, node_purpose, node_type = python_utils.getNodeNameParts(fk_base_controls[i])
+            new_parent_group = cmds.group(fk_base_controls[i], name='{0}_{1}_{2}_{3}_{4}'.format(prefix, component_name, joint_name, 'PAR', 'GRP'))
+            cmds.matchTransform(new_parent_group, fk_base_controls[i], piv=True)
+            python_utils.createMatrixSwitch(fk_rough_controls_1[cur_placement_index], fk_rough_controls_1[next_placement_index], new_parent_group, False, next_control_weight)
 
-        
-
+            
         # Create a locator to hold the ik/fk switch attribute along with whatever else we might need later.
         data_locator = cmds.spaceLocator(name='{0}_{1}_ikfkspine_DAT_LOC'.format(self.prefix, self.name))[0]
         data_locator = cmds.parent(data_locator, fk_joints[0], relative=True)[0]
--- src/crig_maya/utilities/python_utils.py
+++ src/crig_maya/utilities/python_utils.py
@@ -23,17 +23,49 @@ def generate_component_base(component_name, component_prefix):
     groups['placement_group'] = cmds.group(name='{0}_{1}_placement_PLC_GRP'.format(component_prefix, component_name), parent=groups['parent_group'], empty=True)
     return groups
 
-def createMatrixSwitch(parent1, parent2, child):
+def connectTransforms(parent, child):
+    cmds.connectAttr('{0}.translate'.format(parent), '{0}.translate'.format(child))
+    cmds.connectAttr('{0}.rotate'.format(parent), '{0}.rotate'.format(child))
+    cmds.connectAttr('{0}.scale'.format(parent), '{0}.scale'.format(child))
+
+def createMatrixSwitch(parent1, parent2, child, world_matrix=True, weight=0.0):
+    if world_matrix:
+        attr = 'worldMatrix[0]'
+    else:
+        attr = 'matrix'
+    mult_matrix = None
     blend_matrix = cmds.createNode('blendMatrix', name='{0}_BLND_SWTCHM'.format(child))
-    cmds.connectAttr('{0}.worldMatrix[0]'.format(parent1), '{0}.inputMatrix'.format(blend_matrix))
-    cmds.connectAttr('{0}.worldMatrix[0]'.format(parent2), '{0}.target[0].targetMatrix'.format(blend_matrix))
-    mult_matrix, matrix_decompose = constrainByMatrix('{0}.outputMatrix'.format(blend_matrix), child)
+    cmds.connectAttr('{0}.{1}'.format(parent1, attr), '{0}.inputMatrix'.format(blend_matrix))
+    cmds.connectAttr('{0}.{1}'.format(parent2, attr), '{0}.target[0].targetMatrix'.format(blend_matrix))
+    cmds.setAttr('{0}.target[0].weight'.format(blend_matrix), weight)
+    if world_matrix:
+        mult_matrix, matrix_decompose = constrainByMatrix('{0}.outputMatrix'.format(blend_matrix), child)
+    else:
+        input_matrix = '{0}.outputMatrix'.format(blend_matrix)
+        matrix_decompose = decomposeAndConnectMatrix(input_matrix, child)
     return blend_matrix, mult_matrix, matrix_decompose
 
-def constrainTransformByMatrix(parent, child, maintain_offset=False):
+def createColorBlend(attr1, attr2, child, weight=0.0):
+    blend_color = cmds.shadingNode('blendColors', name='{0}_BLND_BLNDC'.format(child), asUtility=True)
+    cmds.connectAttr(attr1, '{0}.color1'.format(blend_color))
+    cmds.connectAttr(attr2, '{0}.color2'.format(blend_color))
+    cmds.setAttr('{0}.blender'.format(blend_color), weight)
+    cmds.connectAttr('{0}.output'.format(blend_color), child)
+    return blend_color
+
+def createScalarBlend(attr1, attr2, child, weight=0.0):
+    blend_color = cmds.shadingNode('blendColors', name='{0}_BLND_BLNDC'.format(child), asUtility=True)
+    cmds.connectAttr(attr1, '{0}.color1R'.format(blend_color))
+    cmds.connectAttr(attr2, '{0}.color2R'.format(blend_color))
+    cmds.setAttr('{0}.blender'.format(blend_color), weight)
+    cmds.connectAttr('{0}.outputR'.format(blend_color), child)
+    return blend_color
+
+def constrainTransformByMatrix(parent, child, maintain_offset=False,):
+    offset_matrix = getLocalOffset(parent, child)
     mult_matrix, matrix_decompose = constrainByMatrix('{0}.worldMatrix'.format(parent), child)
+    
     if maintain_offset:
-        offset_matrix = getLocalOffset(parent, child)
         cmds.setAttr('{0}.matrixIn[0]'.format(mult_matrix), [offset_matrix(i, j) for i in range(4) for j in range(4)], type="matrix")
 
     return mult_matrix, matrix_decompose
@@ -43,24 +75,25 @@ def constrainByMatrix(parentMatrix, childTransform, maintain_offset=False):
 
     cmds.connectAttr(parentMatrix, '{0}.matrixIn[1]'.format(mult_matrix))
     cmds.connectAttr('{0}.parentInverseMatrix[0]'.format(childTransform), '{0}.matrixIn[2]'.format(mult_matrix))
-    matrix_decompose = cmds.createNode('decomposeMatrix', name='{0}_MCNST_DCOMP'.format(childTransform))
-    cmds.connectAttr('{0}.matrixSum'.format(mult_matrix), '{0}.inputMatrix'.format(matrix_decompose))
-    cmds.connectAttr('{0}.outputRotate'.format(matrix_decompose), '{0}.rotate'.format(childTransform))
-    cmds.connectAttr('{0}.outputScale'.format(matrix_decompose), '{0}.scale'.format(childTransform))
-    cmds.connectAttr('{0}.outputTranslate'.format(matrix_decompose), '{0}.translate'.format(childTransform))
+    input_matrix = '{0}.matrixSum'.format(mult_matrix)
+    matrix_decompose = decomposeAndConnectMatrix(input_matrix, childTransform)
 
     if maintain_offset:
-        print(parentMatrix)
         parent_mMatrix = getMMatrixFromList(cmds.getAttr(parentMatrix))
-        print([parent_mMatrix(i, j) for i in range(4) for j in range(4)])
         child_mMatrix = getMMatrixFromList(cmds.getAttr('{0}.worldMatrix'.format(childTransform)))
-        print([child_mMatrix(i, j) for i in range(4) for j in range(4)])
         offset_matrix = child_mMatrix * parent_mMatrix.inverse()
-        print([offset_matrix(i, j) for i in range(4) for j in range(4)])
         cmds.setAttr('{0}.matrixIn[0]'.format(mult_matrix), [offset_matrix(i, j) for i in range(4) for j in range(4)], type="matrix")
 
         
     return mult_matrix, matrix_decompose
+
+def decomposeAndConnectMatrix(inputMatrix, childTransform):
+    matrix_decompose = cmds.createNode('decomposeMatrix', name='{0}_MCNST_DCOMP'.format(childTransform))
+    cmds.connectAttr(inputMatrix, '{0}.inputMatrix'.format(matrix_decompose))
+    cmds.connectAttr('{0}.outputRotate'.format(matrix_decompose), '{0}.rotate'.format(childTransform))
+    cmds.connectAttr('{0}.outputScale'.format(matrix_decompose), '{0}.scale'.format(childTransform))
+    cmds.connectAttr('{0}.outputTranslate'.format(matrix_decompose), '{0}.translate'.format(childTransform))
+    return matrix_decompose
 
 def copyOverMatrix(parentMatrix, childTransform):
     matrix_decompose = cmds.createNode('decomposeMatrix', name='{0}_MCNST_DCOMP'.format(childTransform))
@@ -100,6 +133,10 @@ def getDagPath(node=None):
     selection.getDagPath(0, dagPath)
     return dagPath
 
+def getMObject(node=None):
+    selection = om.MSelectionList()
+    selection.add(node)
+
 def getLocalOffset(parent, child):
     parentWorldMatrix = getDagPath(parent).inclusiveMatrix()
     childWorldMatrix = getDagPath(child).inclusiveMatrix()
@@ -110,6 +147,20 @@ def getMMatrixFromList(list_matrix):
     m_mat = om.MMatrix()
     om.MScriptUtil.createMatrixFromList(list_matrix, m_mat)
     return m_mat
+
+def getTransformDistance(node1, node2):
+    transformFuncs = om.MFnTransform()
+    node1DAG = getDagPath(node1)
+    transformFuncs.setObject(node1DAG)
+    node1Vector = transformFuncs.translation(om.MSpace.kWorld)
+
+    node2DAG = getDagPath(node2)
+    transformFuncs.setObject(node2DAG)
+    node2Vector = transformFuncs.translation(om.MSpace.kWorld)
+
+    distVector = node1Vector - node2Vector
+    return distVector.length()
+
 
 def  makeCircleControl(name, scale):
     curve_points = [[0.7836116248912245, 4.798237340988473e-17, -0.7836116248912246], [6.785732323110912e-17, 6.785732323110912e-17, -1.1081941875543877], [-0.7836116248912246, 4.798237340988472e-17, -0.7836116248912244], [-1.1081941875543881, 3.517735619006027e-33, -5.74489823752483e-17], [-0.7836116248912246, -4.7982373409884725e-17, 0.7836116248912245], [-1.1100856969603225e-16, -6.785732323110917e-17, 1.1081941875543884], [0.7836116248912245, -4.798237340988472e-17, 0.7836116248912244], [1.1081941875543881, -9.253679210110099e-33, 1.511240500779959e-16]]
@@ -132,8 +183,9 @@ def makeDirectControl(name, controlledNode, scale, curveType="circle"):
         control = makeSquareControl(name, scale)
     prefix, component_name, joint_name, node_purpose, node_type = getNodeNameParts(name)
     parent = cmds.listRelatives(controlledNode, parent=True)
-    position_group = cmds.group(control, name='{0}_{1}_{2}_{3}_{4}'.format(prefix, component_name, joint_name, 'PLC', 'GRP'), parent=parent[0])
+    position_group = cmds.group(control, name='{0}_{1}_{2}_{3}_{4}'.format(prefix, component_name, joint_name, 'PLC', 'GRP'))
     cmds.matchTransform(position_group, controlledNode)
+    cmds.parent(position_group, parent[0])
     cmds.parent(controlledNode, control)
     return position_group, control
     
